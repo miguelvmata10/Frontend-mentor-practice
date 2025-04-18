944ee930cb4bcab11098b610558e910b1ce9ad05
miguelmata2003@gmail.com <miguelmata2003@gmail.com>
2025-04-19 00:13:34 +0100

    Calculator project: Refactor in calculator body

--- CalculatorApp/Calculator/src/components/CalculatorBody.tsx
+++ CalculatorApp/Calculator/src/components/CalculatorBody.tsx
@@ -17,52 +17,42 @@ const CalculatorBody = ({ bgColor, textColor, buttonColors}: CalculatorBodyProps
     const [ operation, setOperation ] = useState<ButtonType | null>(null);
     const [ isJustCalculated, setIsJustCalculated ] = useState<boolean>(false);
 
+    const isOperator = (item: ButtonType) => ['+', '-', 'x', '/'].includes(item);
+
     const calculateResult = () => {
         const secondNumber = displayNumber;
         const first = Number(firstNumber ?? '0');
         const second = Number(secondNumber);
-        let result = '';
 
         if (operation === '/' && second === 0) {
             alert('Impossível dividir por zero.');
             return '0'; 
         }
 
-        switch (operation) {
-            case '+':
-                result = String(first + second);
-                break;
-            case '-':
-                result = String(first - second);
-                break;
-            case 'x':
-                result = String(first * second);
-                break;
-            case '/':
-                result = String(first / second);
-                break;
-            default:
-                return '0';
-        }
+        const operationsMap: { [key in ButtonType]?: (a: number, b: number) => number } = {
+            '+': (a, b) => a + b,
+            '-': (a, b) => a - b,
+            'x': (a, b) => a * b,
+            '/': (a, b) => a / b,
+        };
 
-        return result;
+        const result = operationsMap[operation!]?.(first, second);
+        return String(result ?? '0');
     }
 
     const handleButtonClick = (item: ButtonType) => {
-        if (item === 'DEL') {
-            handleDelete();
-        }
-        else if (item === 'RESET') {
-            handleReset();
-        }
-        else if (item === '=') {
-            handleEqual();
-        }
-        else if (item === '+' || item === '-' || item === 'x' || item === '/' ) {
-            handleNumberOperations(item);
-        }
-        else {
-            handleNumberAddition(item);
+        switch(item) {
+            case 'DEL':
+                handleDelete();
+                break;
+            case 'RESET':
+                handleReset();
+                break;
+            case '=':
+                handleEqual();
+                break;
+            default:
+                isOperator(item) ? handleNumberOperations(item) : handleNumberAddition(item);
         }
     }
 
@@ -70,8 +60,8 @@ const CalculatorBody = ({ bgColor, textColor, buttonColors}: CalculatorBodyProps
         if (displayNumber === '0') return;
 
         setDisplayNumber((prev) => {
-            if (prev.length === 1) return '0'; 
-            return prev.slice(0, -1)
+            const newVal = prev.slice(0, -1) || '0';
+            return newVal === '-' ? '0' : newVal
         })
     }
 
@@ -98,13 +88,13 @@ const CalculatorBody = ({ bgColor, textColor, buttonColors}: CalculatorBodyProps
             const result = calculateResult();
             setDisplayNumber(result);
             setFirstNumber(result);
-            setOperation(item);
-            setIsJustCalculated(true);
         } else {
             setFirstNumber(displayNumber);
-            setOperation(item);
-            setDisplayNumber('0');
         }
+
+        setOperation(item);
+        setDisplayNumber('0');
+        setIsJustCalculated(true);
     }
 
     const handleNumberAddition = (item: ButtonType) => {
@@ -124,28 +114,32 @@ const CalculatorBody = ({ bgColor, textColor, buttonColors}: CalculatorBodyProps
 
     return (
         <div className="w-full flex flex-col items-center justify-center gap-4">
-            <div className={`w-full overflow-hidden ${bgColor} rounded-md p-6 ${textColor} flex text-5xl font-bold items-center justify-end`}>
-                {displayNumber}
+            <div className={`w-full ${bgColor} rounded-md p-6 ${textColor} flex flex-col items-end`}>
+                <div className="w-full overflow-x-auto overflow-y-hidden">
+                    <div className="text-5xl font-bold whitespace-nowrap text-right">
+                        {displayNumber}
+                    </div>
+                </div>
             </div>
             <div className={`w-full ${bgColor} rounded-md p-6 flex flex-col gap-4 justify-center items-center`}>
                 {buttons.map((row, rowIndex) => (
-                    <div key={rowIndex} className="w-full flex items-center justify-between gap-1 sm:gap-5">
+                    <div key={rowIndex} className="w-full flex items-center justify-between gap-2 sm:gap-5">
                         {row.map((item, index) => (
                             <button
                                 onClick={() => handleButtonClick(item)} 
                                 key={index}
                                 className={`flex ${row.length === 2 ? 'w-1/2' : 'flex-1'} 
-                                items-center justify-center cursor-pointer font-bold text-3xl px-3 py-2 rounded-lg 
-                                hover:scale-102 duration-300 border-b-4 active:border-b-0 active:duration-initial 
-                                ${
-                                    item === '=' 
-                                    ? buttonColors.equalBtt 
-                                    : ['RESET', 'DEL'].includes(item) 
-                                        ? buttonColors.resetDelBtt
-                                        : buttonColors.numberBtt
-                                }`}>
-                                    {item}
-                          </button>   
+                                    items-center justify-center cursor-pointer font-bold text-2xl sm:text-3xl px-3 py-2 rounded-lg 
+                                    hover:scale-102 duration-300 border-b-4 active:border-b-0 active:duration-initial 
+                                    ${
+                                        item === '=' 
+                                        ? buttonColors.equalBtt 
+                                        : ['RESET', 'DEL'].includes(item) 
+                                            ? buttonColors.resetDelBtt
+                                            : buttonColors.numberBtt
+                                    }`}>
+                                        {item}
+                            </button>   
                         ))}
                     </div>
                 ))}
