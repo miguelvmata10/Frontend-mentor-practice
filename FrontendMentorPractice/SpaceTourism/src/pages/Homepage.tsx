e8d39d43c651161955048f26493dd71e3b66f8d9
miguelmata2003@gmail.com <miguelmata2003@gmail.com>
2025-04-22 23:58:16 +0100

    Homepage menu done

--- FrontendMentorPractice/SpaceTourism/src/pages/Homepage.tsx
+++ FrontendMentorPractice/SpaceTourism/src/pages/Homepage.tsx
@@ -1,28 +1,29 @@
 const Homepage = () => {
   return (
-    <div className="w-full h-full flex flex-col lg:flex-row items-center p-6 md:p-0 md:px-10 md:py-32 lg:p-32">
-      <div className="flex flex-col gap-5 items-center flex-1 lg:justify-start lg:items-start">
-        <h2 className="font-barlow tracking-widest text-lg md:text-2xl uppercase">
-          So, you want to travel to
+    <div className="h-full w-full flex flex-col lg:flex-row lg:items-end p-6 md:p-32">
+      <div className="text-center lg:text-left lg:justify-start flex-1 flex flex-col gap-6">
+        <h2 className="font-barlow tracking-widest uppercase text-lg md:text-2xl">
+          so, you want to travel
         </h2>
-        <h1 className="font-bellefair text-8xl md:text-9xl uppercase">Space</h1>
-        <p className="text-center text-lg md:text-xl font-barlow lg:max-w-lg lg:text-start">
+        <h1 className="font-bellefair uppercase text-7xl md:text-8xl">space</h1>
+        <p className="font-barlow text-[16px] md:text-lg lg:max-w-[540px]">
           Let’s face it; if you want to go to space, you might as well genuinely
           go to outer space and not hover kind of on the edge of it. Well sit
           back, and relax because we’ll give you a truly out of this world
           experience!
         </p>
       </div>
-      <div className="flex flex-1 items-center lg:justify-end">
+
+      <div className="flex-1 flex items-center justify-center lg:justify-end">
         <button
-          className="text-center text-lg md:text-3xl font-bellefair uppercase bg-white text-black 
-          rounded-full w-32 h-32 md:w-[200px] md:h-[200px] 
-          cursor-pointer hover:shadow-[0_0_0_60px_rgba(255,255,255,0.2)] transition duration-250"
+          className="font-bellefair bg-white text-black rounded-full w-36 h-36 md:w-64 md:h-64 md:text-2xl 
+            hover:shadow-[0_0_0_40px_rgba(255,255,255,0.2)] transition-shadow duration-200 cursor-pointer"
         >
-          Explore
+          EXPLORE
         </button>
       </div>
     </div>
   );
 };
+
 export default Homepage;
