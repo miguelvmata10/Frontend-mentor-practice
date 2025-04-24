4892c739200302b25269fb989d9c6542e6155401
miguelmata2003@gmail.com <miguelmata2003@gmail.com>
2025-04-24 23:11:38 +0100

    Link to destination menu in explore button added

--- FrontendMentorPractice/SpaceTourism/src/pages/Homepage.tsx
+++ FrontendMentorPractice/SpaceTourism/src/pages/Homepage.tsx
@@ -1,7 +1,9 @@
+import { useNavigate } from "react-router-dom";
+
 const Homepage = () => {
+  const navigate = useNavigate();
   return (
     <div className="flex-1 flex flex-col justify-between lg:flex-row lg:items-end p-6 md:p-32">
-
       <div className="text-center lg:text-left lg:justify-start flex-1 flex flex-col gap-6">
         <h2 className="font-barlow tracking-widest uppercase text-lg md:text-2xl">
           so, you want to travel
@@ -17,13 +19,13 @@ const Homepage = () => {
 
       <div className="flex-1 flex items-center justify-center lg:justify-end">
         <button
+          onClick={() => navigate("/destination")}
           className="font-bellefair bg-white text-black rounded-full w-36 h-36 md:w-64 md:h-64 md:text-2xl 
-            hover:shadow-[0_0_0_40px_rgba(255,255,255,0.2)] transition-shadow duration-200 cursor-pointer"
+        hover:shadow-[0_0_0_40px_rgba(255,255,255,0.2)] transition-shadow duration-200 cursor-pointer"
         >
           EXPLORE
         </button>
       </div>
-      
     </div>
   );
 };
